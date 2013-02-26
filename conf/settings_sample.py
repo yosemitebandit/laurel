7625d8b4dfce8b13fb1e7f6821c3a40ddb0db4c2
Matt Ball <matt.ball.2@gmail.com>
2013-02-25 23:19:33 -0800

    adds a sample walking tour server

---
+++ conf/settings_sample.py
@@ -0,0 +1,18 @@
+''' config file for the laurel app
+copy this somewhere safe and set an env var:
+    $ export LAUREL_SETTINGS=/path/to/settings/file
+'''
+
+DEBUG = False
+
+HOST = '0.0.0.0'
+PORT = 5005
+
+TWILIO = {
+    'account_sid': 'ACabc123'
+}
+
+POINTS = {
+    1: 'https://s3.amazonaws.com/example/diner.mp3'
+    , 2: 'https://s3.amazonaws.com/example/theater.mp3'
+}
---
+++ serve/serve.py
@@ -0,0 +1,43 @@
+''' serve.py
+simple twilio responder
+'''
+from flask import Flask, request, abort
+
+app = Flask(__name__)
+app.config.from_envvar('LAUREL_SETTINGS')
+
+
+@app.route('/api/1/voice/incoming', methods=['POST'])
+def api_v1_incoming_voice():
+    ''' twilio will post data here when a call is received
+    we respond to fresh calls with a welcome message and instructions
+    post requests also come here after digits are pressed
+    the system will respond to digits with an mp3
+    '''
+    # make sure the post request originated from Twilio
+    if request.form['AccountSid'] != app.config['TWILIO']['account_sid']:
+        abort(403)
+
+    # collect digits entered on the keypad, if any
+    digits = request.form.get('Digits', None)
+
+    if not digits:
+        # this is a brand new call into the system
+        return render_template('welcome.xml')
+    
+    # digits were pressed - find the relevant mp3
+    points = app.config['POINTS']
+    if digits not in points.keys():
+        # requested point not found
+        return render_template('point_not_found.xml')
+
+    # valid submission
+    mp3 = app.config['POINTS'][digits]
+    return render_template('point.xml', mp3=mp3)
+
+
+if __name__ == '__main__':
+    app.run(
+        host = app.config['HOST']
+        , port = int(app.config['PORT'])
+    )
